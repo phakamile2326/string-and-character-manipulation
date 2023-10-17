cb43db191343c8bd8686783d7f346b7256565b75
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-17 09:05:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_and_character_manipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(check("FizzBuzz"));
+            Console.WriteLine(check("Fizz"));
+            Console.WriteLine(check("Buzz"));
+            Console.WriteLine(check("Founder"));
+            Console.ReadLine();
+        }
+        public static string check(string str)
+        {
+            if (str.StartsWith("F") && str.EndsWith("B"))
+            {
+                return "FizzBuzz";
+            }
+            else if (str.StartsWith("F"))
+                return "Fizz";
+            else if (str.EndsWith("B"))
+                return "Buzz";
+            else 
+                return str;
+            
+        }
+    }
+}
